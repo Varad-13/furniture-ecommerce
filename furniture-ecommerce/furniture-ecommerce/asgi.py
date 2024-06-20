0209938c7f3ce3316753b86be06b351eec68fd5e
Varad J <varad.gcs@gmail.com>
2024-06-21 00:17:53 +0530

    Rebrand to furniture ecommerce

--- furniture-ecommerce/furniture-ecommerce/asgi.py
+++ furniture-ecommerce/furniture-ecommerce/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for interviewStack project.
+ASGI config for furniture-ecommerce project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'interviewStack.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'furniture-ecommerce.settings')
 
 application = get_asgi_application()
--- furniture-ecommerce/furniture-ecommerce/wsgi.py
+++ furniture-ecommerce/furniture-ecommerce/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for interviewStack project.
+WSGI config for furniture-ecommerce project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'interviewStack.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'furniture-ecommerce.settings')
 
 application = get_wsgi_application()
