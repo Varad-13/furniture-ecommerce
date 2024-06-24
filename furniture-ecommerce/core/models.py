3d5d7ca5413ec9818668a38b3edf5ffa37fd1f7a
Varad J <varad.gcs@gmail.com>
2024-06-24 13:45:38 +0530

    Initial search view

--- furniture-ecommerce/core/models.py
+++ furniture-ecommerce/core/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Category(models.Model):
+    category_name = models.CharField(max_length=255)
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     discount = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    ratings = models.FloatField()
-    category = models.CharField(max_length=255)
     inventory = models.PositiveIntegerField()
     url = models.URLField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
@@ -27,7 +29,6 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     mobile_number = models.CharField(max_length=15)
     past_orders = models.ManyToManyField('Order', blank=True)
-    ratings = models.ManyToManyField('Product', through='Rating')
 
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -40,19 +41,10 @@ class Order(models.Model):
     payment_method = models.CharField(max_length=50)
     payment_status = models.CharField(max_length=50)
     fulfilment_status = models.CharField(max_length=50)
-    rating = models.FloatField(null=True, blank=True)
     timestamp = models.DateTimeField()
 
-class Discount(models.Model):
+class Coupon(models.Model):
     coupon_code = models.CharField(max_length=50)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    category = models.CharField(max_length=255, null=True, blank=True)
+    user = models.ManyToManyField(User, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='coupons')
     is_common = models.BooleanField(default=False)
-
-class Rating(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    rating = models.FloatField()
-
-    class Meta:
-        unique_together = ('user', 'product')
--- furniture-ecommerce/core/views.py
+++ furniture-ecommerce/core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def search_products(request):
+    query = request.GET.get('q', '')
+    products = Product.objects.filter(name__icontains=query)
+    context = {
+        'query': query,
+        'products': products,
+    }
+    return render(request, 'core/search_results.html', context)
