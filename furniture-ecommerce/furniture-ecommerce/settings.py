c374c33336bf8d01e504ee8435998d45bf610e56
Varad J <varad.gcs@gmail.com>
2024-06-28 03:10:10 +0530

    Import whitenoise middleware

--- furniture-ecommerce/furniture-ecommerce/settings.py
+++ furniture-ecommerce/furniture-ecommerce/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = secret.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -65,6 +65,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     #allauth account middleware
     'allauth.account.middleware.AccountMiddleware',
+    #whitenoise
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 AUTHENTICATION_BACKENDS = [
