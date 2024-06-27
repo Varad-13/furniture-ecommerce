892ebb6eb75acd5755aaa15a2dc15da3f44cf093
Varad J <varad.gcs@gmail.com>
2024-06-28 02:55:37 +0530

    Remove staticfile dirs

--- furniture-ecommerce/furniture-ecommerce/settings.py
+++ furniture-ecommerce/furniture-ecommerce/settings.py
@@ -148,11 +148,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
