6f438a77082c09464bb818743aaef940c3bb96b7
Varad J <varad.gcs@gmail.com>
2024-06-28 02:07:12 +0530

    resolve the staticfiles.E002 error

--- furniture-ecommerce/furniture-ecommerce/settings.py
+++ furniture-ecommerce/furniture-ecommerce/settings.py
@@ -140,19 +140,19 @@ USE_TZ = True
 
 # Media files
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'static'
-
-STATICFILES_DIRS = (
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-)
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
